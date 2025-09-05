0f0e29348a37c3e4d1fdbe9e0b0931a94b2e7497
oda66_9@hotmail.com <oda66_9@hotmail.com>
2025-09-05 17:43:37 -0400

    Ajoute des icons, modifications du carousel et des ajustement sur index, ajoute de l'animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+/*********gsap animation **************************************/
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.fromTo(
+    ".img-logo img",
+    { scale: 0.5, opacity: 3 },
+    { scale: 1, opacity: 1, duration: 10, ease: "elastic.out(0.8, 0.3)" }
+  );
+});
+
 /***********effect pour les card de  la page music ***********/
 
 const cards = document.querySelectorAll(".card");
