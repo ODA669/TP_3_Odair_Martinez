31eedcf95a581d6ebec9c00c34cbbb89c04819b3
oda66_9@hotmail.com <oda66_9@hotmail.com>
2025-09-08 19:33:11 -0400

    ajoute service worker

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 /*******************service worker*****************************/
 
-// Register service worker.
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("service-worker.js").then((reg) => {
-      console.log("Service worker registered.", reg);
-    });
-  });
-}
-
 /*********gsap animation **************************************/
 
 document.addEventListener("DOMContentLoaded", () => {
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,3 +1,21 @@
+//Update cache names any time any of the cached files change.
+const CACHE_NAME = "static-cache-v1";
+//Add list of files to cache here.
+// const FILES_TO_CACHE = [""];
+self.addEventListener("install", (evt) => {
+  console.log("[ServiceWorker] Install");
+  // Precache static resources here.
+  self.skipWaiting();
+});
+self.addEventListener("activate", (evt) => {
+  console.log("[ServiceWorker] Activate");
+  //Remove previous cached data from disk.
+  self.clients.claim();
+});
+self.addEventListener("fetch", (evt) => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
+  //Add fetch event handler here.
+});
 //Add list of files to cache here.
 const FILES_TO_CACHE = ["offline.html"];
 self.addEventListener("install", (evt) => {
@@ -10,4 +28,20 @@ self.addEventListener("install", (evt) => {
     })
   );
   self.skipWaiting();
+});
+
+self.addEventListener("fetch", (evt) => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
+  //Add fetch event handler here.
+  if (evt.request.mode !== "navigate") {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match("/Cochenille/PointNClick/offline . h t m l ");
+      });
+    })
+  );
 });
