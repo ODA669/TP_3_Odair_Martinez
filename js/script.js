7d4e27809f2bed20094bb9d0f2e97ba81008b5c3
oda66_9@hotmail.com <oda66_9@hotmail.com>
2025-09-08 18:57:57 -0400

    modification page music

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/*******************service worker*****************************/
+
+// Register service worker.
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js").then((reg) => {
+      console.log("Service worker registered.", reg);
+    });
+  });
+}
+
 /*********gsap animation **************************************/
 
 document.addEventListener("DOMContentLoaded", () => {
