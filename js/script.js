3227d8089731083fffc8bdfa88f089b6f7eb6705
oda66_9@hotmail.com <oda66_9@hotmail.com>
2025-09-16 15:04:31 -0400

    modifications service worker

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
-/*******************service worker*****************************/
-
-/*********gsap animation **************************************/
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   gsap.fromTo(
-//     ".img-logo img",
-//     { scale: 0.5, opacity: 3 },
-//     { scale: 1, opacity: 1, duration: 10, ease: "elastic.out(0.8, 0.3)" }
-//   );
-// });
-
-/**Ajustement du debut de video en seconds* */
-const video = document.getElementById("bg-video");
-
-// charge de video duration
-video.addEventListener("loadedmetadata", () => {
-  video.currentTime = 4.5; //le video star en 4.5 secondes
+/**section index video */
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.fromTo(
+    ".img-logo img",
+    { scale: 0.5, opacity: 3 },
+    { scale: 1, opacity: 1, duration: 10, ease: "elastic.out(0.8, 0.3)" }
+  );
 });
+/*****section maps contact */
+const { animate } = window.framerMotion;
 
-gsap.from("#form", { opacity: 0, y: -50, duration: 1 });
-gsap.from("#map", { opacity: 0, x: 50, duration: 1, delay: 0.5 });
-gsap.from("#about", { opacity: 0, y: 50, duration: 1, delay: 1 });
-gsap.from("#services", { opacity: 0, y: 50, duration: 1, delay: 1.3 });
-gsap.from("#faq", { opacity: 0, y: 50, duration: 1, delay: 1.6 });
+animate("#form", { opacity: [0, 1], y: [-50, 0] }, { duration: 1 });
+animate("#map", { opacity: [0, 1], x: [50, 0] }, { duration: 1, delay: 0.5 });
+animate("#about", { opacity: [0, 1], y: [50, 0] }, { duration: 1, delay: 1 });
+animate(
+  "#services",
+  { opacity: [0, 1], y: [50, 0] },
+  { duration: 1, delay: 1.3 }
+);
+animate("#faq", { opacity: [0, 1], y: [50, 0] }, { duration: 1, delay: 1.6 });
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tp3-cache-v1";
+const CACHE_NAME = "tp3-cache-v2";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
